7477138d42bfba32228ca62ad8a0e1f46dac206a
Julianne Yang <julianneyangcollege@gmail.com>
2023-05-29 15:26:35 -0700

    deleted everything

--- 2021-8-Microbiome-Batch-Correction-Analysis/RPCA_LuminalvsMucosal_PcoA/principal-coordinates-2022.sh
+++
@@ -1,3 +0,0 @@
-echo "enter file path for distance matrix .qza" $1
-qiime diversity pcoa --i-distance-matrix $1 --o-pcoa "${1%.qza}_pcoa.qza" 
-
--- README.md
+++
@@ -1,3 +0,0 @@
-# Mouse Biogeography Scripts 
-
-Raw data associated with this project can be found on NCBI with accession `PRJNA944800`.
