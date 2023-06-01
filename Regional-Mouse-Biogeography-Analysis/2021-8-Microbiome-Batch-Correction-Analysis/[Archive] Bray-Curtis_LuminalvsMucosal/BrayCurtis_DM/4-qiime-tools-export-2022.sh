1803be1ebe0a34a103d9e2f84f3865507ac9121b
Julianne Yang <julianneyangcollege@gmail.com>
2023-06-01 15:34:53 -0700

    deleted irrelevant files

--- Regional-Mouse-Biogeography-Analysis/2021-8-Microbiome-Batch-Correction-Analysis/[Archive] Bray-Curtis_LuminalvsMucosal/BrayCurtis_DM/4-qiime-tools-export-2022.sh
+++
@@ -1,7 +0,0 @@
-echo "Enter the path to the qza input file." $1 
-qiime tools export --input-path $1 --output-path  "${1%.qza}_export"
-cd $("${1%}_export")
-biom summarize-table -i feature-table.biom -o biom-summary.txt 
-biom convert -i feature-table.biom -o feature-table.tsv --to-tsv --header-key taxonomy
- 
-
