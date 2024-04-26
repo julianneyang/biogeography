4c5022e0aa8a3edb3325c7dc0b651507b20c4989
Julianne <julianneyangcollege@gmail.com>
2024-04-25 22:34:39 -0400

    Finished running Maaslin2 for phylum level comparisons

--- MouseBiogeography-RProj/Maaslin2_L2_TYPE_all.R
+++ MouseBiogeography-RProj/Maaslin2_L2_TYPE_all.R
@@ -190,184 +190,170 @@ for (site in names(site_general_paths)) {
 }
 
 ## SPF Gavage --
-input_data <- read.csv("Maaslin2_L2/SPF_Gavage/Maaslin2_Type_L2/SPF_Maaslin2_L2_SITE_and_TYPE - L2-Colon.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/SPF_Gavage/Maaslin2_Type_L2/SPF_Maaslin2_L2_SITE_and_TYPE - L2-SI.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/SPF_Gavage/Maaslin2_Type_L2/SPF_Maaslin2_L2_SITE_and_TYPE - L2-Duo.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/SPF_Gavage/Maaslin2_Type_L2/SPF_Maaslin2_L2_SITE_and_TYPE - L2-Jejunum.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/SPF_Gavage/Maaslin2_Type_L2/SPF_Maaslin2_L2_SITE_and_TYPE - L2-Ileum.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/SPF_Gavage/Maaslin2_Type_L2/SPF_Maaslin2_L2_SITE_and_TYPE - L2-Cecum.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/SPF_Gavage/Maaslin2_Type_L2/SPF_Maaslin2_L2_SITE_and_TYPE - L2-PC.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/SPF_Gavage/Maaslin2_Type_L2/SPF_Maaslin2_L2_SITE_and_TYPE - L2-DC.csv", header=TRUE, row.names=1) 
+input_data <- read.delim(here("Humanized-Biogeography-Analysis/starting_files/export_L2_min10000_Cedars_SPF_Colonized-ComBat-Adjusted-ASV/feature-table.tsv"), header=TRUE,row.names=1) 
+df_input_data<-as.data.frame(input_data)
+names(df_input_data)<-gsub("X","",names(df_input_data))
+df_input_data <- df_input_data %>% select(-c(taxonomy))
+input_metadata <-read.delim(here("Humanized-Biogeography-Analysis/starting_files/Humanized-Metadata.tsv"),header=TRUE, row.names=1) #mapping file
+row.names(input_metadata)
+row.names(input_metadata)<-gsub("-",".",row.names(input_metadata))
+input_metadata$SampleID <- row.names(input_metadata)
 
-df_input_data <- as.data.frame(input_data)
-df_input_data <- select(df_input_data, -c("taxonomy"))
+samples<- input_metadata %>%
+  filter(SampleID %in% names(df_input_data)) %>%
+  pull(SampleID)
 
-input_metadata <-read.csv("Humanized-Biogeography-Analysis/Humanized Metadata - All-Humanized-Metadata (1).csv",header=TRUE, row.names=1) #mapping file
-
+df_input_data <- df_input_data[,samples]
 target <- colnames(df_input_data)
 input_metadata = input_metadata[match(target, row.names(input_metadata)),]
 target == row.names(input_metadata)
 
-df_input_metadata<-input_metadata
-df_input_metadata$MouseID <- factor(df_input_metadata$MouseID)
+input_metadata$SampleID <- row.names(input_metadata)
+df_input_metadata <- as.data.frame(input_metadata)
 df_input_metadata$Sequencing_Run <- factor(df_input_metadata$Sequencing_Run)
+df_input_metadata$MouseID <- factor(df_input_metadata$MouseID)
 df_input_metadata$Sex <- factor(df_input_metadata$Sex)
+df_input_metadata$Site <- factor(df_input_metadata$Site)
 df_input_metadata$Type <- factor(df_input_metadata$Type, levels=c("Luminal", "Mucosal"))
-df_input_metadata$Site_General <- factor(df_input_metadata$Site_General, levels=c("Colon","SI"))
-df_input_metadata$Site <- factor(df_input_metadata$Site, levels=c("Distal_Colon", "Proximal_Colon", "Cecum", "Ileum","Jejunum","Duodenum"))
-
 sapply(df_input_metadata,levels)
 
-#Colon
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/SPF_Gavage/L2-Colon-CLR-ComBat-SeqRunSexSiteType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Site","Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+# List of sites and corresponding file paths
+site_paths <- list(
+  Duodenum = "Humanized-Biogeography-Analysis/differential_genera_type/L2-LumRef-CLR-Duodenum-ComBat-SeqRunSexType-1-MsID",
+  Jejunum = "Humanized-Biogeography-Analysis/differential_genera_type/L2-LumRef-CLR-Jejunum-ComBat-SeqRunSexType-1-MsID",
+  Ileum = "Humanized-Biogeography-Analysis/differential_genera_type/L2-LumRef-CLR-Ileum-ComBat-SeqRunSexType-1-MsID",
+  Cecum = "Humanized-Biogeography-Analysis/differential_genera_type/L2-LumRef-CLR-Cecum-ComBat-SeqRunSexType-1-MsID",
+  Proximal_Colon = "Humanized-Biogeography-Analysis/differential_genera_type/L2-LumRef-CLR-PC-ComBat-SeqRunSexType-1-MsID",
+  Distal_Colon = "Humanized-Biogeography-Analysis/differential_genera_type/L2-LumRef-CLR-DC-ComBat-SeqRunSexType-1-MsID"
+)
 
-#SI
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/SPF_Gavage/L2-SI-CLR-ComBat-SeqRunSexSiteType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Site","Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+site_general_paths <- list(
+  SI = "Humanized-Biogeography-Analysis/differential_genera_type/L2-LumRef-CLR-SI-ComBat-SeqRunSexSiteType-1-MsID",
+  Colon = "Humanized-Biogeography-Analysis/differential_genera_type/L2-LumRef-CLR-Colon-ComBat-SeqRunSexSiteType-1-MsID"
+)
 
-#Duodenum
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/SPF_Gavage/L2-Duodenum-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+# Iterate over each site
+site_fe <- c("Sequencing_Run", "Sex", "Type")
+ranef <- c("MouseID")
+refs <- c("Sequencing_Run,One","Site,Distal_Colon")
 
-#Jejunum
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/SPF_Gavage/L2-Jejunum-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+for (site in names(site_paths)) {
+  # Get sample IDs for the current site
+  samples <- df_input_metadata %>% 
+    filter(Site == site, SampleID %in% names(df_input_data)) %>% 
+    pull(SampleID)
+  
+  # Subset input data for the current site
+  input_data <- df_input_data[, samples]
+  
+  # Run Maaslin2 analysis
+  output_path <- paste0(site_paths[[site]])
+  fit_data <- run_maaslin2(input_data, output_path, site, site_fe,ranef,refs)
+}
 
-#Ileum
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/SPF_Gavage/L2-Ileum-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+site_general_fe <- c("Sequencing_Run","Sex", "Site","Type")
+for (site in names(site_general_paths)) {
+  # Get sample IDs for the current site
+  samples <- df_input_metadata %>% 
+    filter(Site_General == site, SampleID %in% names(df_input_data)) %>% 
+    pull(SampleID)
+  
+  # Subset input data for the current site
+  input_data <- df_input_data[, samples]
+  
+  # Run Maaslin2 analysis
+  output_path <- paste0(site_general_paths[[site]])
+  fit_data <- run_maaslin2(input_data, output_path, site, site_general_fe,ranef,refs)
+}
 
-#Cecum
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/SPF_Gavage/L2-Cecum-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
 
-#Proximal Colon
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/SPF_Gavage/L2-PC-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
-#Distal Colon
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/SPF_Gavage/L2-DC-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
 ## HUM Gavage --
-input_data <- read.csv("Maaslin2_L2/HUM_Gavage/HUM_Maaslin2_SITE_and_TYPE_L2 - Colon_L2.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/HUM_Gavage/HUM_Maaslin2_SITE_and_TYPE_L2 - SI_L2.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/HUM_Gavage/HUM_Maaslin2_SITE_and_TYPE_L2 - Duodenum_L2.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/HUM_Gavage/HUM_Maaslin2_SITE_and_TYPE_L2 - Jejunum_L2.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/HUM_Gavage/HUM_Maaslin2_SITE_and_TYPE_L2 - Ileum_L2.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/HUM_Gavage/HUM_Maaslin2_SITE_and_TYPE_L2 - Cecum_L2.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/HUM_Gavage/HUM_Maaslin2_SITE_and_TYPE_L2 - PC_L2.csv", header=TRUE, row.names=1) 
-input_data <- read.csv("Maaslin2_L2/HUM_Gavage/HUM_Maaslin2_SITE_and_TYPE_L2 - DC_L2.csv", header=TRUE, row.names=1) 
+input_data <- read.delim(here("Humanized-Biogeography-Analysis/starting_files/export_L2_min10000_Humanized_Colonized-ComBat-Adjusted-ASV/feature-table.tsv"), header=TRUE,row.names=1) 
+df_input_data<-as.data.frame(input_data)
+names(df_input_data)<-gsub("X","",names(df_input_data))
+df_input_data <- df_input_data %>% select(-c(taxonomy))
+input_metadata <-read.delim(here("Humanized-Biogeography-Analysis/starting_files/Humanized-Metadata.tsv"),header=TRUE, row.names=1) #mapping file
+row.names(input_metadata)
+row.names(input_metadata)<-gsub("-",".",row.names(input_metadata))
+input_metadata$SampleID <- row.names(input_metadata)
 
-df_input_data <- as.data.frame(input_data)
-df_input_data <- select(df_input_data, -c("taxonomy"))
-input_metadata <-read.csv("Humanized-Biogeography-Analysis/Humanized Metadata - All-Humanized-Metadata (1).csv",header=TRUE, row.names=1) #mapping file
+samples<- input_metadata %>%
+  filter(SampleID %in% names(df_input_data)) %>%
+  pull(SampleID)
 
+df_input_data <- df_input_data[,samples]
 target <- colnames(df_input_data)
 input_metadata = input_metadata[match(target, row.names(input_metadata)),]
 target == row.names(input_metadata)
-names(input_metadata)
 
-df_input_metadata<-input_metadata
-df_input_metadata$MouseID <- factor(df_input_metadata$MouseID)
+input_metadata$SampleID <- row.names(input_metadata)
+df_input_metadata <- as.data.frame(input_metadata)
 df_input_metadata$Sequencing_Run <- factor(df_input_metadata$Sequencing_Run)
+df_input_metadata$MouseID <- factor(df_input_metadata$MouseID)
 df_input_metadata$Sex <- factor(df_input_metadata$Sex)
+df_input_metadata$Site <- factor(df_input_metadata$Site)
 df_input_metadata$Type <- factor(df_input_metadata$Type, levels=c("Luminal", "Mucosal"))
-df_input_metadata$Site_General <- factor(df_input_metadata$Site_General, levels=c("Colon","SI"))
-df_input_metadata$Site <- factor(df_input_metadata$Site, levels=c("Distal_Colon", "Proximal_Colon", "Cecum", "Ileum","Jejunum","Duodenum"))
-
 sapply(df_input_metadata,levels)
 
-#Colon
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/HUM_Gavage/L2-Colon-CLR-ComBat-SeqRunSexSiteType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Site","Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+# List of sites and corresponding file paths
+site_paths <- list(
+  Jejunum = "Humanized-Biogeography-Analysis/differential_genera_type/HUM_L2-LumRef-CLR-Jejunum-ComBat-SeqRunSexType-1-MsID",
+  Ileum = "Humanized-Biogeography-Analysis/differential_genera_type/HUM_L2-LumRef-CLR-Ileum-ComBat-SeqRunSexType-1-MsID",
+  Cecum = "Humanized-Biogeography-Analysis/differential_genera_type/HUM_L2-LumRef-CLR-Cecum-ComBat-SeqRunSexType-1-MsID",
+  Proximal_Colon = "Humanized-Biogeography-Analysis/differential_genera_type/HUM_L2-LumRef-CLR-PC-ComBat-SeqRunSexType-1-MsID",
+  Distal_Colon = "Humanized-Biogeography-Analysis/differential_genera_type/HUM_L2-LumRef-CLR-DC-ComBat-SeqRunSexType-1-MsID"
+)
 
-#SI
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/HUM_Gavage/L2-SI-CLR-ComBat-SeqRunSexSiteType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Site","Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+site_general_paths <- list(
+  SI = "Humanized-Biogeography-Analysis/differential_genera_type/HUM_L2-LumRef-CLR-SI-ComBat-SeqRunSexSiteType-1-MsID",
+  Colon = "Humanized-Biogeography-Analysis/differential_genera_type/HUM_L2-LumRef-CLR-Colon-ComBat-SeqRunSexSiteType-1-MsID"
+)
 
-#Duodenum
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/HUM_Gavage/L2-Duodenum-CLR-ComBat-SexType-1-MsID", 
-                    fixed_effects = c("Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
 
-#Jejunum
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/HUM_Gavage/L2-Jejunum-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+# Iterate over each site
+site_fe <- c("Sequencing_Run", "Sex", "Type")
+duod_fe <- c("Sex","Type")
+ranef <- c("MouseID")
+refs <- c("Sequencing_Run,One","Site,Distal_Colon")
 
-#Ileum
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/HUM_Gavage/L2-Ileum-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+for (site in names(site_paths)) {
+  # Get sample IDs for the current site
+  samples <- df_input_metadata %>% 
+    filter(Site == site, SampleID %in% names(df_input_data)) %>% 
+    pull(SampleID)
+  
+  # Subset input data for the current site
+  input_data <- df_input_data[, samples]
+  
+  # Run Maaslin2 analysis
+  output_path <- paste0(site_paths[[site]])
+  fit_data <- run_maaslin2(input_data, output_path, site, site_fe,ranef,refs)
+}
 
-#Cecum
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/HUM_Gavage/L2-Cecum-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+  # For duodenum only because sex and seq run are conflated
+  samples <- df_input_metadata %>% 
+    filter(Site == "Duodenum", SampleID %in% names(df_input_data)) %>% 
+    pull(SampleID)
+  
+  # Subset input data for the current site
+  input_data <- df_input_data[, samples]
+  
+  # Run Maaslin2 analysis
+  output_path <- "Humanized-Biogeography-Analysis/differential_genera_type/HUM_L2-LumRef-CLR-Duodenum-ComBat-SeqRunSexType-1-MsID"
+  fit_data <- run_maaslin2(input_data, output_path, site, duod_fe,ranef,refs)
 
-#Proximal Colon
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/HUM_Gavage/L2-PC-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
-#Distal Colon
-fit_data = Maaslin2(input_data=df_input_data, input_metadata=df_input_metadata, 
-                    output = "Maaslin2_L2/HUM_Gavage/L2-DC-CLR-ComBat-SeqRunSexType-1-MsID", 
-                    fixed_effects = c("Sequencing_Run","Sex", "Type"), 
-                    random_effects = c("MouseID"),
-                    normalization="clr", transform ="none",
-                    plot_heatmap = FALSE,plot_scatter = FALSE)
+
+site_general_fe <- c("Sequencing_Run","Sex", "Site","Type")
+for (site in names(site_general_paths)) {
+  # Get sample IDs for the current site
+  samples <- df_input_metadata %>% 
+    filter(Site_General == site, SampleID %in% names(df_input_data)) %>% 
+    pull(SampleID)
+  
+  # Subset input data for the current site
+  input_data <- df_input_data[, samples]
+  
+  # Run Maaslin2 analysis
+  output_path <- paste0(site_general_paths[[site]])
+  fit_data <- run_maaslin2(input_data, output_path, site, site_general_fe,ranef,refs)
+}
